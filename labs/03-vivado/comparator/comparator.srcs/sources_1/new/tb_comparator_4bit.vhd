663e5397f0bcf64e7ca05a9aec529ba784f3f198
student <student@PC-634.urel.feec.vutbr.cz>
2022-03-01 16:17:25 +0100

    new assignment added

---
+++ labs/03-vivado/comparator/comparator.srcs/sources_1/new/tb_comparator_4bit.vhd
@@ -0,0 +1,117 @@
+------------------------------------------------------------
+--
+-- Testbench for 2-bit binary comparator.
+-- EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_comparator_4bit is
+    -- Entity of testbench is always empty
+end entity tb_comparator_4bit;
+
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_comparator_4bit is
+
+    -- Local signals
+    signal s_a           : std_logic_vector(4 - 1 downto 0);
+    signal s_b           : std_logic_vector(4 - 1 downto 0);
+    signal s_B_greater_A : std_logic;
+    signal s_B_equals_A  : std_logic;
+    signal s_B_less_A    : std_logic;
+
+begin
+    -- Connecting testbench signals with comparator_2bit
+    -- entity (Unit Under Test)
+    uut_comparator_4bit : entity work.comparator_4bit
+        port map(
+            a_i           => s_a,
+            b_i           => s_b,
+            B_greater_A_o => s_B_greater_A,
+            B_equals_A_o  => s_B_equals_A,
+            B_less_A_o    => s_B_less_A
+        );
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+    p_stimulus : process
+    begin
+        -- Report a note at the beginning of stimulus process
+        report "Stimulus process started" severity note;
+
+        -- First test case ...
+        s_b <= "0000"; s_a <= "0000"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '1') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 0000, 0000 FAILED" severity error;
+        
+        
+        -- Second test case ...
+        s_b <= "0011"; s_a <= "1100"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 0011, 1100 FAILED" severity error;
+
+
+        -- Third test case ...
+        s_b <= "1010"; s_a <= "0101"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '1') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 1010, 0101 FAILED" severity error;
+
+
+        -- Fourth test case ...
+        s_b <= "0111"; s_a <= "1000"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 0111, 1000 FAILED" severity error;
+
+
+        -- Fifth test case - numbers 9 and 3, last from my ID ...
+        s_b <= "1001"; s_a <= "0011"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '1') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 1001, 0011 FAILED" severity error;
+        -- WRITE OTHER TEST CASES HERE
+
+
+
+        -- Report a note at the end of stimulus process
+        report "Stimulus process finished" severity note;
+        wait; -- Data generation process is suspended forever
+    end process p_stimulus;
+
+end architecture testbench;
+
